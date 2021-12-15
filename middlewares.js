944b18c234df2caa83731ae44baeae241b46558b
TatuMon <ezurkanmn@gmail.com>
2021-12-15 18:48:27 -0300

    Fixed connect.ejs style

--- middlewares.js
+++ middlewares.js
@@ -3,7 +3,7 @@
 //TODO: Check if user has token. Kinda like a auth middleware
 const authorized = (req, res, next) => {
     if(!(req.signedCookies['tk'])){
-        res.sendFile('/views/connect.html', { root: global.basePath });
+        res.render('connect');
         return;
     }
 
