f0747dcc1a076b8219c6282242e5ec215de67fa1
TatuMon <ezurkanmn@gmail.com>
2022-05-17 21:38:48 -0300

    Improved (i think) repo structure. Removed tailwind cdn, using jquery package, using bootstrap package, using express-ejs-layout package

--- dist/app.js
+++ dist/app.js
@@ -12,26 +12,39 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+/////  IMPORTS  /////
 const express_1 = __importDefault(require("express"));
 const path_1 = __importDefault(require("path"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const expressLayouts = require('express-ejs-layouts');
 const app = (0, express_1.default)();
 const utils = require('./js/utils');
 const middlewares = require('./middlewares');
 const { httpsRequest } = require('./js/https');
-const oauth = require('./routes/oauth');
-const user = require('./routes/user');
+const oauthRouter = require('./routes/oauth');
+const userRouter = require('./routes/user');
+/////  IMPORTS  /////
+//View engine settings
+app.use(expressLayouts);
+app.set('layout', path_1.default.join(__dirname, '/views/layouts/default'));
 app.set('view engine', 'ejs');
 app.set('views', path_1.default.join(__dirname, '/views'));
-app.use('*/css', express_1.default.static(path_1.default.join(__dirname, 'public/css')));
+//Static files
+app.use('/lib/bootstrap/css', express_1.default.static(path_1.default.join(__dirname, '../node_modules/bootstrap/dist/css')));
+app.use('/lib/bootstrap/js', express_1.default.static(path_1.default.join(__dirname, '../node_modules/bootstrap/dist/js')));
+app.use('/lib/jquery', express_1.default.static(path_1.default.join(__dirname, '../node_modules/jquery/dist')));
+app.use('/css', express_1.default.static(path_1.default.join(__dirname, 'public/css')));
+app.use('/js', express_1.default.static(path_1.default.join(__dirname, 'public/js')));
+//Cookies!
 app.use((0, cookie_parser_1.default)(process.env.COOKIE_SECRET));
-//The about page is open to anyone
+//Routes that don't need access token
 app.get('/about', (req, res) => {
     res.render('about');
 });
-app.use(oauth);
+app.use(oauthRouter);
+//From now on, you need the access token to use these routes
 app.use(middlewares.authorized);
-app.use(user);
+app.use(userRouter);
 app.get('/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     let accessToken = req.signedCookies['tk'];
     let options = {
--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,45 @@
+/////  IMPORTS  /////
 import express from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
+
+const expressLayouts = require('express-ejs-layouts');
 
 const app = express();
 const utils = require('./js/utils');
 const middlewares = require('./middlewares');
 const { httpsRequest } = require('./js/https')
 
-const oauth = require('./routes/oauth');
-const user = require('./routes/user');
+const oauthRouter = require('./routes/oauth');
+const userRouter = require('./routes/user');
+/////  IMPORTS  /////
 
+//View engine settings
+app.use(expressLayouts);
+app.set('layout', path.join(__dirname, '/views/layouts/default'));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
-app.use('*/css', express.static(path.join(__dirname, 'public/css')));
+//Static files
+app.use('/lib/bootstrap/css', express.static(path.join(__dirname, '../node_modules/bootstrap/dist/css')));
+app.use('/lib/bootstrap/js', express.static(path.join(__dirname, '../node_modules/bootstrap/dist/js')));
+app.use('/lib/jquery', express.static(path.join(__dirname, '../node_modules/jquery/dist')));
+app.use('/css', express.static(path.join(__dirname, 'public/css')));
+app.use('/js', express.static(path.join(__dirname, 'public/js')));
+
+//Cookies!
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
-//The about page is open to anyone
+//Routes that don't need access token
 app.get('/about', (req, res) => {
     res.render('about');
 })
+app.use(oauthRouter);
 
-app.use(oauth);
+//From now on, you need the access token to use these routes
 app.use(middlewares.authorized);
-app.use(user);
 
+app.use(userRouter);
 app.get('/', async (req, res) => {
     let accessToken = req.signedCookies['tk'];
 
